ed6987344cd28123e380a08f488ea2d697c0019c
Harshit Agarwal <harshit1110@gmail.com>
2025-08-02 19:00:31 +0000

    increase size of company name

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -62,7 +62,7 @@ function HomePageContent() {
         </div>
         <div className="flex-1 flex justify-center items-center gap-4">
           <Image src="/logo.png" alt="Logo" width={50} height={50} />
-          <h1 className="font-headline text-4xl font-bold tracking-tight text-accent whitespace-nowrap">
+          <h1 className="font-headline text-5xl font-bold tracking-tight text-accent whitespace-nowrap">
             {companyInfo.name}
           </h1>
         </div>
